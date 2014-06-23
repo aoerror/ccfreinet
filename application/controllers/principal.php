2273e3615c1e3ba9a2f2a96a0532cb5c9ffafb8c
luis <mluism@live.com.mx>
2014-06-22 21:20:23 -0500

    Prueba 1

---
+++ application/controllers/principal.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Principal extends CI_Controller {
+
+	public function index()
+	{
+		echo "hoasidh";
+	}
+
+}
+
+/* End of file principal.php */
+/* Location: ./application/controllers/principal.php */
